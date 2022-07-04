848ba84ec47deab151ecaae6360658e00c4def91
Debangshu_Saha <sahdebsaha909@gmail.com>
2022-07-04 22:21:10 +0530

    Update stack01.c

--- stack01.c
+++ stack01.c
@@ -1,8 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct stack
+{
+    int size;
+    int top;
+    int *arr; 
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if(ptr->top==-1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(struct stack *ptr)
+{
+    if(ptr->top==ptr->size-1)
+    {
+        return 1;
+    }
+    return 0;
+}
 
 int main()
 {
+    struct stack *s;
+    s->size=80;
+    s->top=-1;
+    s->arr=(int *)malloc(s->size * sizeof(int));
+    //Adding values to stack manually
+
+    s->arr[0]=7;
+    s->top++;
+
+    if(isEmpty(s))
+    {
+        printf("The stack is Empty\n ");
+    }
+    else
+    printf("The stack is not empty\n ");
     return 0;
 }
