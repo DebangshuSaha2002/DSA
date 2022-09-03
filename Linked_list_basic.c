9617e8617bc2a92a3f2faf04e4ae42e21e7b6a21
Debangshu_Saha <sahdebsaha909@gmail.com>
2022-09-03 23:02:01 +0530

    All single linked list operations
    This program performs all the insertion and deletion operations on a single linked list.
    It uses switch case to accept the users choice and performs the operations.
    
    Note- Head pointer is declared as global

--- Linked_list_basic.c
+++ Linked_list_basic.c
@@ -5,23 +5,115 @@ struct node
 {
     int data;
     struct node *next;
-};
+}*head=NULL;
 
-void insert_at_node(struct node *head,int data,int index)
+void linkedListTraversal(struct node *ptr);
+void insert_first();
+void insert_last();
+void insert_pos(int pos);
+void del_first();
+void del_last();
+void del_pos(int pos);
+
+void insert_first()
 {
-    struct node *ptr=(struct node *)malloc(sizeof(struct node));
-    struct node *p=(struct node *)malloc(sizeof(struct node));
-    p=head;
-    int i=0;
-    while(i!=index-1)
-    {
-        p=p->next;
-        i++;
-    }
-    ptr->data=data;
-    ptr->next=p->next;
-    p->next=ptr;
+    printf("Inserting element at the first: \n");
+    struct node *new;
+    new=(struct node *)malloc(sizeof(struct node));
+    printf("Enter the value: ");
+    scanf("%d",&new->data);
+    new->next=head;
+    head=new;
+    linkedListTraversal(head);
 }
+
+void insert_last()
+{
+    printf("Inserting element at the last: \n");
+    struct node *new;
+    new=(struct node *)malloc(sizeof(struct node));
+    struct node *ptr;
+
+    printf("Enter a value: ");
+    scanf("%d",&new->data);
+    ptr=head;
+    while (ptr->next!=NULL)
+    {
+        ptr=ptr->next;
+    }
+    ptr->next=new;
+    linkedListTraversal(head);
+}
+
+void insert_pos(int pos)
+{
+    printf("Inserting at a position: \n");
+    struct node *new;
+    new=(struct node *)malloc(sizeof(struct node));
+    struct node *ptr;
+
+    printf("Enter a value: ");
+    scanf("%d",&new->data);
+    ptr=head;
+    int count=1;
+    while (ptr!=NULL && count!=pos-1)
+    {
+        ptr=ptr->next;
+        count+=1;
+    }
+    new->next=ptr->next;
+    ptr->next=new;
+    linkedListTraversal(head);
+}
+
+void del_first()
+{
+    printf("Deleting the first element: \n");
+    struct node *ptr;
+    ptr=head;
+    if(head==NULL)
+    {
+        printf("Deletion not possible ");
+        exit(0);
+    }
+    head=head->next;
+    free(ptr);
+    printf("\n");
+    linkedListTraversal(head);
+}
+
+void del_last()
+{
+    struct node * ptr;
+    struct node * temp;
+    ptr=head;
+
+    while((ptr->next)->next!=NULL)
+    {
+        ptr=ptr->next;
+    }
+    ptr->next=NULL;
+    
+}
+
+void del_pos(int pos)
+{
+    int count=1;
+    struct node *temp;
+    struct node *ptr;
+    ptr=head;
+    while(ptr!=NULL && count!=pos-1)
+    {
+        ptr=ptr->next;
+        count++;
+    }
+    // ptr->next=temp->next;
+    temp=ptr->next;
+    ptr->next=temp->next;
+    linkedListTraversal(head);
+    free(temp);
+}
+
 
 void linkedListTraversal(struct node* ptr)
 {
@@ -33,7 +125,7 @@ void linkedListTraversal(struct node* ptr)
 }
 
 
-void adding_tothe_ll(struct node *ptr,struct node *head, struct node *new)
+void adding_tothe_ll(struct node *ptr, struct node *new)
 {
     char ch;
     printf("Do you want to add (y,n) \n");
@@ -53,10 +145,10 @@ void adding_tothe_ll(struct node *ptr,struct node *head, struct node *new)
 
 int main()
 {
-    struct node *head;
+    //struct node *head;
     struct node *new;
     struct node *ptr;
-    head=NULL;
+    //head=NULL;
 
     new=(struct node *)malloc(sizeof(struct node));
     printf("Enter the value: \n");
@@ -64,12 +156,71 @@ int main()
     new->next=NULL;
     head=new;
     ptr=head;
+    int num;
+    printf("\n1:Add numbers to the linked list\n2:Insert element at first\n3:Insert element at last\n4.Insert element at a position\n");
+    printf("5.Delete element at the first\n6.Delete element at the last\n7.Delete element at a position\n");
+    int i=0;
+    while(i!=1){
+    printf("Enter your choice: ");
+    scanf("%d",&num);
 
-    adding_tothe_ll(ptr,head,new);
-    linkedListTraversal(ptr);
-    insert_at_node(head,101,2);
-    printf("\n");
-    linkedListTraversal(ptr);
+    switch (num)
+    {
+        case 1:
+        {
+            adding_tothe_ll(ptr,new);
+            linkedListTraversal(ptr);
+            printf("\n");
+            break;
+        }
+        case 2:
+        {
+            insert_first();
+            printf("\n");
+            break;
+        }
+        case 3:
+        {
+            insert_last();
+            printf("\n");
+            break;
+        }
+        case 4:
+        {
+            int n1;
+            printf("Enter the position where u want to add the element: ");
+            scanf("%d",&n1);
+            insert_pos(n1);
+            printf("\n");
+            break;
+        }
+        case 5:
+        {
+            del_first();
+            printf("\n");
+            break;
+        }
+        case 6:
+        {
+            del_last();
+            printf("\n");
+            break;
+        }
+        case 7:
+        {
+            int n2;
+            printf("Enter the position whose value u want to delete: ");
+            scanf("%d",&n2);
+            del_pos(n2);
+            printf("\n");
+            break;
+        }
+        default:
+        {
+            i=1;
+        }
+    }
+    }
     return 0;
 }
 
