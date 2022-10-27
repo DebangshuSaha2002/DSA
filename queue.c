f852926ddcbde932deed7ea81792ae47115d321b
Debangshu_Saha <sahdebsaha909@gmail.com>
2022-10-27 16:35:53 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,4 +1,5 @@
 //QUEUE using array 
+
 #include<stdio.h>
 int rear=-1;
 int front=-1;
@@ -7,10 +8,10 @@ int queue[5];
 
 void traverse()
 {
-    if(rear=front=-1)
+    if(rear==-1 && front==-1)
     {
         printf("There is no element in the queue\n");
-        exit(0);
+        return;
     }
     else
     {
@@ -27,11 +28,11 @@ void ENQUEUE()
     if(rear==maxsize)
     {
         printf("OVERFLOW\n");
-        exit(0);
+        return ;
     }
     printf("Enter the value you want to insert: ");
     scanf("%d",&val);
-    if(front=rear=-1)
+    if(front==-1 && rear==-1)
     {
         front=rear=0;
         queue[rear]=val;
@@ -41,7 +42,7 @@ void ENQUEUE()
         rear=rear+1;
         queue[rear]=val;
     }
-    traverse();
+    // traverse();
 }
 
 void DEQUEUE()
@@ -50,24 +51,29 @@ void DEQUEUE()
     if(rear==maxsize)
     {
         printf("UNDERFLOW");
-        exit(0);
+        return ;
     }
-    else if(front==rear)
+    if(front==rear)
     {
         val=queue[front];
         front=rear=-1;
     }
+    else
+    {
+        val=queue[front];
+        front ++;
+    }
     printf("The DEQUEUED Value is : %d\n",val);
-    traverse();
+    // traverse();
 }
 
 void PEEK()
 {
     int p;
-    if(rear==front==(-1))
+    if(rear==-1 && front==-1)
     {
         printf("QUEUE is Empty\n");
-        exit(0);
+        return ;
     }
     else
     {
@@ -79,7 +85,11 @@ void PEEK()
 int main()
 {
     ENQUEUE();
+    ENQUEUE();
+    ENQUEUE();
+    traverse();
     DEQUEUE();
+    traverse();
     PEEK();
     return 0;
 }
